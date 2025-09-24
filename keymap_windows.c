a0bc3e1897425b2b84acbbb69f16be13a9d3bac3
Blake Walters <blake@markupboy.com>
2025-09-24 16:19:55 -0600

    fixed spacing on windows layout

--- keymap_windows.c
+++ keymap_windows.c
@@ -24,10 +24,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MO(1)  , KC_LGUI, KC_LALT,                            KC_SPC ,                   KC_RALT, MO(1)  ,          KC_LEFT, KC_DOWN, KC_RGHT
 ),
 [1] = LAYOUT_65_ansi_blocker(
-    KC_GRV , KC_F1     , KC_F2  , KC_F3  , KC_F4     , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, KC_MUTE,
-    _______, _______   , _______, _______, _______   , _______, _______, _______, _______, _______, KC_PSCR, KC_SCRL, _______, _______, KC_END,
-    KC_CAPS, _______   , _______, _______, _______   , _______, _______, _______, _______, _______, _______, _______, _______         , KC_VOLU,
-    _______, LCAG(KC_Z), _______, _______, LCAG(KC_Z), QK_BOOT, _______, _______, _______, _______, _______, _______,          KC_PGUP, KC_VOLD,
-    _______, _______   , _______,                            KC_MPLY,                   _______, _______,          KC_HOME, KC_PGDN, KC_END
+    KC_GRV , KC_F1     , KC_F2  , KC_F3  , KC_F4     , KC_F5     , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, KC_MUTE,
+    _______, _______   , _______, _______, _______   , LCAG(KC_T), _______, _______, _______, _______, KC_PSCR, KC_SCRL, _______, _______, KC_END,
+    KC_CAPS, _______   , _______, _______, _______   , _______   , _______, _______, _______, _______, _______, _______, _______         , KC_VOLU,
+    _______, LCAG(KC_Z), _______, _______, LCAG(KC_Z), QK_BOOT   , _______, _______, _______, _______, _______, _______,          KC_PGUP, KC_VOLD,
+    _______, _______   , _______,                               KC_MPLY,                      _______, _______,          KC_HOME, KC_PGDN, KC_END
 )
 };
